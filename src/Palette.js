4395ec9067f64c96bc33e3d92fd8b4a9f6ca0eee
Mocktar <mocktarissa@gmail.com>
2020-03-03 18:17:52 +0300

    created palette component

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div>
+            Palette
+        </div> );
+    }
+}
+ 
+export default Palette;
